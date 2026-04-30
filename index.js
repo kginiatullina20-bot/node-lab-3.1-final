f9a9a3223598a1dcb1c56f7b27a70591157b988c
kamsi <giniatullina.0@mail.ru>
2026-04-30 22:11:10 +0300

    Initial commit: Node.js app for Lab 3.1

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Giniyatullina Kamila! Version 1.0');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
